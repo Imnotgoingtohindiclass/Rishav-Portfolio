dca9f22f62e68e540d36001600a4c155100a8e7b
r.15.hav <104674705+Imnotgoingtohindiclass@users.noreply.github.com>
2025-04-03 21:37:49 +0800

    R. E. F. O. R. M. E. D.

--- Penis/src/scene/Background.tsx
+++
@@ -1,8 +0,0 @@
-// Simple black background
-const Background = () => {
-  return (
-    <color attach="background" args={["#141414"]} />
-  );
-};
-
-export default Background;
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
+import React from "react";
 import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "@/components/ui/toaster";
-import NotFound from "@/pages/not-found";
-import Home from "@/pages/home";
-import ProjectDetail from "@/pages/project-detail";
-import ArticleDetail from "@/pages/article-detail";
-import LoadingScreen from "@/components/layout/loading-screen";
+import { Toaster } from "./components/ui/toaster";
+import NotFound from "./pages/not-found";
+import Home from "./pages/home";
+import ProjectDetail from "./pages/project-detail";
+import ArticleDetail from "./pages/article-detail";
+import LoadingScreen from "./components/layout/loading-screen";
 import { useEffect, useState } from "react";
 
 function Router() {
